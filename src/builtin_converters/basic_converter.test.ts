2c5e41cb5aa48995b329a1f9d3e47683850711c2
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-18 23:36:18 -0700

    feat: add some tests for basic converter

---
+++ src/builtin_converters/basic_converter.test.ts
@@ -0,0 +1,28 @@
+import { WowUrlConverter, WowUrlConverterConfig } from "wow-interface";
+import { BasicWowLinkConverter } from "./basic_converter";
+
+describe("Basic converter tests", () => {
+    test("initialize basic converter", () => {
+        const config: WowUrlConverterConfig = {
+            fetcherResponse: {
+                wowMapping: {}
+            }
+        };
+        const converter: WowUrlConverter = new BasicWowLinkConverter(config);
+        expect(converter).toBeDefined();
+    });
+
+    test("convert exact match", () => {
+        const config: WowUrlConverterConfig = {
+            fetcherResponse: {
+                wowMapping: {
+                    "gh": "https://github.com"
+                }
+            }
+        };
+        const converter: WowUrlConverter = new BasicWowLinkConverter(config);
+        expect(converter.convert({
+            wowUrl: "gh"
+        }).fullUrl).toMatch("https://github.com");
+    });
+});
