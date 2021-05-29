11f5538196bc7039909868cb36594e8602bd3482
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-29 14:44:10 -0700

    fix: format some code and rename to extact matcher instead of basic (#12)

---
+++ src/builtin_converters/exact/converter.test.ts
@@ -0,0 +1,52 @@
+import {
+  WowUrlConverter,
+  WowUrlConverterConfig,
+  WowUrlRankingItem,
+} from "@wowlink/wow-interface";
+import { BasicWowLinkConverter } from "./converter";
+
+describe("Basic converter tests", () => {
+  test("initialize basic converter", () => {
+    const config: WowUrlConverterConfig = {
+      fetcherResponse: {
+        wowMapping: {},
+      },
+    };
+    const converter: WowUrlConverter = new BasicWowLinkConverter(config);
+    expect(converter).toBeDefined();
+  });
+
+  test("convert exact match", () => {
+    const config: WowUrlConverterConfig = {
+      fetcherResponse: {
+        wowMapping: {
+          gh: "https://github.com",
+        },
+      },
+    };
+    const converter: WowUrlConverter = new BasicWowLinkConverter(config);
+    expect(
+      converter.convert({
+        wowUrl: "gh",
+      }).fullUrl
+    ).toMatch("https://github.com");
+  });
+
+  test("convert fuzzy match", () => {
+    const config: WowUrlConverterConfig = {
+      fetcherResponse: {
+        wowMapping: {
+          gh: "https://github.com",
+          random: "https://some/random/url.com",
+        },
+      },
+    };
+    const converter: WowUrlConverter = new BasicWowLinkConverter(config);
+    const conversionRes = converter.convert({ wowUrl: "github" });
+    expect(conversionRes.searchRanking.length).toBeGreaterThan(0);
+    const matchItem: WowUrlRankingItem = conversionRes.searchRanking[0];
+    expect(matchItem.fullUrl).toMatch("https://github.com");
+    expect(matchItem.wowUrl).toMatch("gh");
+    expect(matchItem.score).toBeGreaterThan(0);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import {
-    WowUrlConverter,
-    WowUrlConverterConfig,
-    BuiltInWowUrlConverterType
+  WowUrlConverter,
+  WowUrlConverterConfig,
+  BuiltInWowUrlConverterType,
 } from "@wowlink/wow-interface";
-import { BasicWowLinkConverter } from "./builtin_converters/basic_converter";
+import { BasicWowLinkConverter } from "./builtin_converters/exact/converter";
 
-const WowUrlConverterFactory = (type: BuiltInWowUrlConverterType, config: WowUrlConverterConfig): WowUrlConverter => {
-    if (type == BuiltInWowUrlConverterType.Basic) {
-        return new BasicWowLinkConverter(config);
-    } else {
-        throw new Error(`Unknown converter type ${type}.`);
-    }
+const WowUrlConverterFactory = (
+  type: BuiltInWowUrlConverterType,
+  config: WowUrlConverterConfig
+): WowUrlConverter => {
+  // TODO(@tianhaoz95): rename this to EXACT instead of BASIC to be more informative.
+  if (type == BuiltInWowUrlConverterType.Basic) {
+    return new BasicWowLinkConverter(config);
+  } else {
+    throw new Error(`Unknown converter type ${type}.`);
+  }
 };
 
 export { WowUrlConverterFactory };
